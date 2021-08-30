5b2527bedcea6958a548e8851056189b0889e8b3
yahialabeeb <yahia.labeeb@gmail.com>
2021-08-30 05:19:25 +0300

    edit

--- horned-beasts/src/contant/Main.js
+++ horned-beasts/src/contant/Main.js
@@ -5,8 +5,8 @@ class Main extends React.Component {
         return (
             <main>
                 <HornedBeasts
-                title="UniWhal"
-                imgurl= "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" 
+                title="Unicorn Head"
+                imgurl=  "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" 
                 describtion= "A unicorn and a narwhal nuzzling their horns" />
                 <HornedBeasts />
             </main>
